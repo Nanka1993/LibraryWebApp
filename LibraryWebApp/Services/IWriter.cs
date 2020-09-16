7351e82fd95307dd282420696e4e2fc2f42d667c
Nana <Nana@LAPTOP-G8GTO9EJ>
2020-09-16 18:43:22 +0300

    + IWriter<T>

---
+++ LibraryWebApp/Services/IWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryWebApp.Services
+{
+    public interface  IWriter<T> : IDisposable
+    {
+        void Create(T entity);
+    }
+
+}
