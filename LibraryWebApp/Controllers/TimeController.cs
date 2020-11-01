7a26da495ed815ab96f753bef5d2fd1bb6f4abe5
Nana <Nana@LAPTOP-G8GTO9EJ>
2020-11-01 19:02:05 +0300

    + Добавили контроллер TimeController

---
+++ LibraryWebApp/Controllers/TimeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryWebApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        private readonly ITimeAcquirer _timeAcquirer;
+
+        public TimeController(ITimeAcquirer timeAcquirer)
+        {
+            _timeAcquirer = timeAcquirer;
+        }
+
+        [HttpGet]
+        public Task<DateTime> GetCurrentTimeAsync()
+        {
+            return _timeAcquirer.GetCurrentAsync(HttpContext.RequestAborted);
+        }
+    }
+}
---
+++ LibraryWebApp/Services/TimeAcquirer.cs
@@ -0,0 +1,56 @@
+﻿using Flurl;
+using Flurl.Http;
+using Newtonsoft.Json;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibraryWebApp.Services
+{
+    public interface ITimeAcquirer
+    {
+        Task<DateTime> GetCurrentAsync(CancellationToken token);
+    }
+
+    public class WcaTimeAcquirer : ITimeAcquirer
+    {
+        public async Task<DateTime> GetCurrentAsync(CancellationToken token)
+        {
+            var decide = await DecideAsync();
+
+            await DoSomethingAsync();
+
+            var result = await Url.Combine(@"http://worldclockapi.com/api/json/utc/now")
+                .GetJsonAsync<WcaResponse>(cancellationToken: token);
+            if (decide)
+            {
+                return DateTime.UtcNow;
+            }
+            return result.CurrentDateTime;
+        }
+
+        private Task DoSomethingAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        private Task<bool> DecideAsync()
+        {
+            return Task.FromResult(true);
+        }
+
+    }
+
+    public class WcaResponse
+    {
+        [JsonProperty("$id")] public string Id { get; set; }
+        public DateTime CurrentDateTime { get; set; }
+        public string UtcOffset { get; set; }
+        public bool IsDayLightSavingsTime { get; set; }
+        public string DayOfTheWeek { get; set; }
+        public string TimeZoneName { get; set; }
+        public long CurrentFileTime { get; set; }
+        public string OrdinalDate { get; set; }
+        public object ServiceResponse { get; set; }
+    }
+}
